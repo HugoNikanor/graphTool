26417385ad8246505ed70072a0898fb6e634c42a
Hugo <hugo.hornquist@gmail.com>
2016-09-22 23:30:40 +0200

    graphScene now adds canvas, but of size 0

--- src/graphCanvas/GraphScene.java
+++ src/graphCanvas/GraphScene.java
@@ -2,11 +2,16 @@ package graphCanvas;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 
 public class GraphScene extends Scene {
 
 	public GraphScene(Parent root) {
 		super(root);
+
+		((Pane) root)
+			.getChildren()
+			.add(new GraphToolCanvas());
 	}
 
 }
