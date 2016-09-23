a2b5d5551313e04a536c83020148001487960d7b
Hugo Hörnquist <hugo.hornquist@gmail.com>
2016-09-23 17:06:44 +0200

    graph scene slightly better laid out

--- src/graphCanvas/GraphScene.java
+++ src/graphCanvas/GraphScene.java
@@ -2,6 +2,7 @@ package graphCanvas;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
 public class GraphScene extends Scene {
@@ -9,9 +10,18 @@ public class GraphScene extends Scene {
 	public GraphScene(Parent root) {
 		super(root);
 
-		((Pane) root)
+		Pane rootPane = (Pane) root;
+
+		rootPane.getStyleClass().add("my-root");
+
+		System.out.println(rootPane.getStyleClass());
+
+		BorderPane borderPane = new BorderPane();
+		borderPane.setCenter(new GraphToolCanvas());
+
+		rootPane
 			.getChildren()
-			.add(new GraphToolCanvas());
+			.add(borderPane);
 	}
 
 }
