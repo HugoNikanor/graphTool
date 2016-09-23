41b9c3444269e6c6eb882748a57d56faad983971
Hugo Hörnquist <hugo.hornquist@gmail.com>
2016-09-23 17:07:22 +0200

    GraphSceneFatory now attaches css to file

--- src/graphCanvas/GraphSceneFactory.java
+++ src/graphCanvas/GraphSceneFactory.java
@@ -1,5 +1,7 @@
 package graphCanvas;
 
+import java.io.File;
+
 import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 
@@ -8,6 +10,11 @@ public class GraphSceneFactory {
 	public static GraphScene makeGraphScene() {
 		Parent root = new StackPane();
 		GraphScene scene = new GraphScene(root);
+
+		File styleFile = new File("resources/stylesheets/canvas.css");
+		String styleFilePath
+			= "file:///" + styleFile.getAbsolutePath().replace("\\", "/");
+		scene.getStylesheets().add(styleFilePath);
 
 		return scene;
 	}
